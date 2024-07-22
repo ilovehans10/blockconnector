22941c49716e015d696fa0cc75a56bbe946ef376
Hans Larsson <hanandlia@gmail.com>
2024-07-22 11:23:23 -0500

    make game boards randomize

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 use std::fmt::Display;
 
+use rand::{distributions::Standard, prelude::Distribution};
+
 const HEIGHT: u8 = 8;
 const WIDTH: u8 = 8;
 
 #[derive(Clone, Copy)]
 enum TileTypes {
     Block(Color),
-    Firework(Direction),
-    DiscoBall,
+    _Firework(Direction),
+    _DiscoBall,
 }
 
 #[derive(Clone, Copy)]
 enum Color {
     Red,
-    Blue,
     Green,
+    Blue,
     Yellow,
 }
 
@@ -30,20 +32,32 @@ impl TileTypes {
 impl Display for TileTypes {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::DiscoBall => write!(f, "*"),
-            Self::Firework(_direction) => todo!(),
+            Self::_DiscoBall => write!(f, "*"),
+            Self::_Firework(_direction) => todo!(),
             Self::Block(color) => match color {
                 Color::Red => write!(f, "R"),
                 Color::Blue => write!(f, "B"),
                 Color::Green => write!(f, "G"),
                 Color::Yellow => write!(f, "Y"),
-            }
+            },
+        }
+    }
+}
+
+impl Distribution<Color> for Standard {
+    fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> Color {
+        match rng.gen_range(0..=3) {
+            0 => Color::Red,
+            1 => Color::Green,
+            2 => Color::Blue,
+            _ => Color::Yellow,
         }
     }
 }
 
 fn main() {
-    let game_board = vec![TileTypes::new(); (HEIGHT * WIDTH).into()];
+    let mut game_board = vec![TileTypes::new(); (HEIGHT * WIDTH).into()];
+    game_board = game_board.into_iter().map(|_| TileTypes::Block(rand::random())).collect();
     println!("╔{}╗", "═".repeat(WIDTH.into()));
     for current_row in game_board.chunks(WIDTH.into()) {
         print!("║");

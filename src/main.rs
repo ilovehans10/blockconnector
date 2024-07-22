55ff376d15f6ae5490666f9a6f89e54ca4b50872
Hans Larsson <hanandlia@gmail.com>
2024-07-22 15:45:59 -0500

    move game board logic into seperate struct and methods

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,38 @@ use termion::color;
 const HEIGHT: u16 = 8;
 const WIDTH: u16 = 16;
 
-fn main() {
-    let mut game_board = vec![TileTypes::new(); (HEIGHT * WIDTH).into()];
-    game_board.fill_with(|| TileTypes::Block(rand::random::<BlockColor>()));
-    println!("╔{}╗", "═".repeat((WIDTH * 2).into()));
-    for current_row in game_board.chunks(WIDTH.into()) {
-        print!("║");
-        for current_element in current_row {
-            print!("{current_element}");
+struct GameData {
+    game_board: Vec<TileTypes>,
+    turn_number: u8,
+    max_turns: u8,
+}
+
+impl GameData {
+    fn new(height: u16, width: u16) -> Self {
+        let mut prototype_board = vec![TileTypes::new(); (height * width).into()];
+        prototype_board.fill_with(|| TileTypes::Block(rand::random::<BlockColor>()));
+        Self {
+            game_board: prototype_board,
+            turn_number: 0,
+            max_turns: 0,
         }
-        println!("{}║", color::Bg(color::Reset));
     }
-    println!("╚{}╝", "═".repeat((WIDTH * 2).into()));
+    fn draw_board(&self) {
+        println!("╔{}╗", "═".repeat((WIDTH * 2).into()));
+        for current_row in self.game_board.chunks(WIDTH.into()) {
+            print!("║");
+            for current_element in current_row {
+                print!("{current_element}");
+            }
+            println!("{}║", color::Bg(color::Reset));
+        }
+        println!("╚{}╝", "═".repeat((WIDTH * 2).into()));
+
+    }
+}
+
+fn main() {
+    let mut game = GameData::new(HEIGHT, WIDTH);
+    game.draw_board();
+    game.draw_board();
 }

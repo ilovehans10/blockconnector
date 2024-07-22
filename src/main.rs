42db9d033d427d19533e124a07e202a4fb2b3d58
Hans Larsson <hanandlia@gmail.com>
2024-07-22 15:29:52 -0500

    migrate tiles code to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,9 @@
-use std::fmt::Display;
-
-use rand::{distributions::Standard, prelude::Distribution};
+mod tiles;
+use crate::tiles::{BlockColor, TileTypes};
 use termion::color;
 
 const HEIGHT: u16 = 8;
 const WIDTH: u16 = 16;
-
-#[derive(Clone, Copy)]
-enum TileTypes {
-    Block(BlockColor),
-    _Firework(Direction),
-    _DiscoBall,
-}
-
-#[derive(Clone, Copy)]
-enum BlockColor {
-    Red,
-    Green,
-    Blue,
-    Yellow,
-}
-
-#[derive(Clone, Copy)]
-enum Direction {
-    _Vertical,
-    _Horizontal,
-}
-
-impl TileTypes {
-    const fn new() -> Self {
-        Self::Block(BlockColor::Blue)
-    }
-}
-
-impl Display for TileTypes {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::_DiscoBall => write!(f, "*"),
-            Self::_Firework(_direction) => todo!(),
-            Self::Block(color) => match color {
-                BlockColor::Red => write!(f, "{}[]", color::Bg(color::Red)),
-                BlockColor::Blue => write!(f, "{}[]", color::Bg(color::Blue)),
-                BlockColor::Green => write!(f, "{}[]", color::Bg(color::Green)),
-                BlockColor::Yellow => write!(f, "{}[]", color::Bg(color::Yellow)),
-            },
-        }
-    }
-}
-
-impl Distribution<BlockColor> for Standard {
-    fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> BlockColor {
-        match rng.gen_range(0..=3) {
-            0 => BlockColor::Red,
-            1 => BlockColor::Green,
-            2 => BlockColor::Blue,
-            _ => BlockColor::Yellow,
-        }
-    }
-}
 
 fn main() {
     let mut game_board = vec![TileTypes::new(); (HEIGHT * WIDTH).into()];

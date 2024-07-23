f6c81708eddb2358d7350e6775480bbdf5df5f21
Hans Larsson <hanandlia@gmail.com>
2024-07-23 07:45:19 -0500

    move game board logic into module

---
+++ src/game_board.rs
@@ -0,0 +1,35 @@
+use crate::tiles::{BlockColor, TileTypes};
+use termion::color;
+
+pub struct GameData {
+        game_board: Vec<TileTypes>,
+        turn_number: u8,
+        max_turns: u8,
+        height: u16,
+        width: u16,
+    }
+
+    impl GameData {
+        pub fn new(height: u16, width: u16) -> Self {
+            let mut prototype_board = vec![TileTypes::new(); (height * width).into()];
+            prototype_board.fill_with(|| TileTypes::Block(rand::random::<BlockColor>()));
+            Self {
+                game_board: prototype_board,
+                turn_number: 0,
+                max_turns: 0,
+                height,
+                width,
+            }
+        }
+        pub fn draw_board(&self) {
+            println!("╔{}╗", "═".repeat((self.width * 2).into()));
+            for current_row in self.game_board.chunks(self.width.into()) {
+                print!("║");
+                for current_element in current_row {
+                    print!("{current_element}");
+                }
+                println!("{}║", color::Bg(color::Reset));
+            }
+            println!("╚{}╝", "═".repeat((self.width * 2).into()));
+        }
+    }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,9 @@
+use crate::game_board::GameData;
+mod game_board;
 mod tiles;
-use crate::tiles::{BlockColor, TileTypes};
-use termion::color;
 
 const HEIGHT: u16 = 8;
 const WIDTH: u16 = 16;
-
-struct GameData {
-    game_board: Vec<TileTypes>,
-    turn_number: u8,
-    max_turns: u8,
-}
-
-impl GameData {
-    fn new(height: u16, width: u16) -> Self {
-        let mut prototype_board = vec![TileTypes::new(); (height * width).into()];
-        prototype_board.fill_with(|| TileTypes::Block(rand::random::<BlockColor>()));
-        Self {
-            game_board: prototype_board,
-            turn_number: 0,
-            max_turns: 0,
-        }
-    }
-    fn draw_board(&self) {
-        println!("╔{}╗", "═".repeat((WIDTH * 2).into()));
-        for current_row in self.game_board.chunks(WIDTH.into()) {
-            print!("║");
-            for current_element in current_row {
-                print!("{current_element}");
-            }
-            println!("{}║", color::Bg(color::Reset));
-        }
-        println!("╚{}╝", "═".repeat((WIDTH * 2).into()));
-
-    }
-}
 
 fn main() {
     let mut game = GameData::new(HEIGHT, WIDTH);

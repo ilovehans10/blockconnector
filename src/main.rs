7d2ec1e0e9a0955b392ac09afb5aca8af82a26a5
Hans Larsson <hanandlia@gmail.com>
2024-09-12 11:05:44 -0500

    remove some old manual test from main

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,13 @@ const WIDTH: u16 = 16;
 fn main() {
     let mut game = GameData::new(HEIGHT, WIDTH);
     let rectangle = Shape::new(&ShapeType::Rectangle(Cordinate::new(8, 4)));
-    println!("{:?}", game.get_cell(Cordinate::new(0, 0)).unwrap());
-    println!("{:?}", game.get_cell(Cordinate::new(1, 1)).unwrap());
     game.draw_info();
+    println!();
     game.apply_shape(&rectangle, Cordinate::new(4, 2), tiles::TileTypes::Gap)
         .unwrap();
     game.set_cell(Cordinate::new(4, 0), tiles::TileTypes::Empty)
         .unwrap();
-    game.set_cell(Cordinate::new(4, 7), tiles::TileTypes::Gap)
-        .unwrap();
-    game.draw_raw();
     game.draw_info();
+    println!();
     game.draw_board();
 }
